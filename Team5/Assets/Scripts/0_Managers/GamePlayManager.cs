6500c9f6de43e5478b577342473af24f34bd6204
김중현 <110381888+gotkagovkfl@users.noreply.github.com>
2024-10-25 23:35:13 +0900

    중현 끝

--- Team5/Assets/Scripts/0_Managers/GamePlayManager.cs
+++ Team5/Assets/Scripts/0_Managers/GamePlayManager.cs
@@ -23,7 +23,7 @@ public class GamePlayManager : Singleton<GamePlayManager>
     [SerializeField] GameOverPanel gameOverPanel;   //게임오버 패널
     [SerializeField] TextMeshProUGUI text_timer;   //게임오버 패널
 
-    [SerializeField] float instantDeathTime = 120;
+    [SerializeField] float instantDeathTime = 180;
     bool instantDeathCalled;
 
     
