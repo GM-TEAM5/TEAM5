684da1cf4b98bc8834125ee5cbdea15d538f4668
안지홍 <hongsam0102@gmail.com>
2024-12-08 01:02:04 +0900

    [기능추가] 데미지 이펙트 시간 변수 상수로 관리

--- Team5/Assets/Scripts/3_Main/2_GamePlay/1_Effect/DamageText.cs
+++ Team5/Assets/Scripts/3_Main/2_GamePlay/1_Effect/DamageText.cs
@@ -24,6 +24,21 @@ public class DamageText : MonoBehaviour, IPoolObject
     private static float lastTextTime;
     private static int activeTextCount = 0;
 
+    // 등장 딜레이
+    private const float APPEAR_DELAY_BASE = 0.05f;
+
+    // Fade In 시간
+    private const float FADE_IN_TIME = 0.15f;
+
+    // 표시 지속 시간
+    private const float DISPLAY_TIME_BASE = 0.5f;
+
+    // Fade Out 시간
+    private const float FADE_OUT_TIME = 0.3f;
+
+    // 위로 올라가는 거리 (위로 쌓임)
+    private const float MOVE_UP_DISTANCE = 1f;
+
     public void OnCreatedInPool()
     {
         text = GetComponent<TextMeshPro>();
@@ -46,7 +61,6 @@ public class DamageText : MonoBehaviour, IPoolObject
         lastTextTime = Time.time;
         activeTextCount++;
 
-        // 초기에는 투명하게 시작
         text.color = new Color(1, 1, 1, 0);
         transform.position = hitPoint + new Vector3(0, lastYOffset, 0);
 
@@ -65,9 +79,9 @@ public class DamageText : MonoBehaviour, IPoolObject
                 break;
         }
 
-        float appearDelay = 0.1f * (activeTextCount - 1);
-        float fadeDelay = 1.0f + (0.1f * (activeTextCount - 1));
-        PlayAnim_MoveAndFade(appearDelay, fadeDelay, textColor);
+        float appearDelay = APPEAR_DELAY_BASE * (activeTextCount - 1);
+        float displayTime = DISPLAY_TIME_BASE + (APPEAR_DELAY_BASE * (activeTextCount - 1));
+        PlayAnim_MoveAndFade(appearDelay, displayTime, textColor);
     }
 
     public void Init(Vector3 hitPoint, float damage, DamageType type = DamageType.DMG_NORMAL)
@@ -75,7 +89,7 @@ public class DamageText : MonoBehaviour, IPoolObject
         Init(hitPoint, damage.ToString("0"), type);
     }
 
-    void PlayAnim_MoveAndFade(float appearDelay, float fadeDelay, Color textColor)
+    void PlayAnim_MoveAndFade(float appearDelay, float displayTime, Color textColor)
     {
         DOTween.Sequence()
             .OnComplete(() =>
@@ -83,11 +97,11 @@ public class DamageText : MonoBehaviour, IPoolObject
                 PoolManager.Instance.TakeToPool<DamageText>(this);
                 activeTextCount--;
             })
-            .AppendInterval(appearDelay * 0.5f)
-            .Append(text.DOColor(textColor, 0.15f))
-            .AppendInterval(fadeDelay * 0.5f)
-            .Append(transform.DOLocalMoveY(transform.localPosition.y + 1f, 0.3f))
-            .Join(text.DOFade(0, 0.3f))
+            .AppendInterval(appearDelay)
+            .Append(text.DOColor(textColor, FADE_IN_TIME))
+            .AppendInterval(displayTime)
+            .Append(transform.DOLocalMoveY(transform.localPosition.y + MOVE_UP_DISTANCE, FADE_OUT_TIME))
+            .Join(text.DOFade(0, FADE_OUT_TIME))
             .Play();
     }
 }
